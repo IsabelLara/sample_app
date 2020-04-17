f885512ac0bcd3e1d0ad24e267c6c3b57fc78b86
IsabelLara <isabeldmlara@gmail.com>
2020-04-17 15:23:15 -0300

    email validation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,15 +112,16 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'attsampleapp.herokuapp.com'
+  host = 'https://attsampleapp.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
-    :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :user_name => ENV["GMAIL_USERNAME"],
+    :password => ENV["GMAIL_PASSWORD"],
+    :authentication => 'plain',
     :enable_starttls_auto => true
   }
   
